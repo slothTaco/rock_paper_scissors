fe2b3e0ac08a382f5881a483b64f2bb4c990d127
SlothTaco <pickeringwilliamscott@gmail.com>
2025-02-18 08:40:07 -0600

    created new file 'computerchoice.js' moved getComputerChoice function from RPS.js to computerchoice.js

---
+++ computerchoice.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+    let compChoices = ["ROCK", "PAPER", "SCISSORS"]
+    // Let computer pick from options: rock paper or scissors.
+    let randomCompChoice = Math.floor(Math.random() * compChoices.length) + 1
+    // randomize options: rock, paper, scissors.
+ 
+    if (randomCompChoice === 1) {
+     return "ROCK";
+    } 
+    if (randomCompChoice === 2) {
+     return "PAPER";
+    }
+    if (randomCompChoice === 3) {
+     return "SCISSORS"
+    }
+    
+ }
