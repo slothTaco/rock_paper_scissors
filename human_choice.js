9c0412e639eb386f15d07b8b03596b437657abfd
SlothTaco <pickeringwilliamscott@gmail.com>
2025-02-18 09:18:57 -0600

    gethumanchoice prompts user for rock paper or scissors and returns their input

---
+++ human_choice.js
@@ -0,0 +1,7 @@
+function getHumanChoice() {
+//Takes player choice of Rock, paper and scissors.
+// will return players choice.
+    let humanChoice = prompt("Pick Rock, paper or scissors! ")
+    return humanChoice;
+
+}
